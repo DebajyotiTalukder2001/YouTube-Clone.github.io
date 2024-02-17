1e38e65cf10fbb65f1e907c3d4e1215ccdbc5184
DebajyotiTalukder2001 <136104351+DebajyotiTalukder2001@users.noreply.github.com>
2024-02-17 10:10:03 +0530

    YouTube Clone

--- src/pages/Modal.tsx
+++ src/pages/Modal.tsx
@@ -12,10 +12,10 @@ export default function Modal() {
                     >
                         <div className="relative mx-auto p-2 w-full max-w-2xl max-h-full">
                             {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-50 shadow bg-gray-700 dark:bg-gray-700 outline-none focus:outline-none">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-50 shadow bg-gray-700 outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-gray-400 rounded-t">
-                                    <h3 className="text-2xl font-semibold text-gray-900 text-white dark:text-white">
+                                    <h3 className="text-2xl font-semibold text-gray-900 text-white">
                                         Disclaimer
                                     </h3>
                                     <button
@@ -39,14 +39,14 @@ export default function Modal() {
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-4 md:p-5 border-t border-solid border-gray-400 rounded-b">
                                     <button
-                                        className="py-2.5 px-5 ms-3 mx-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                        className="py-2.5 px-5 ms-3 mx-3 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700"
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
                                         Close
                                     </button>
                                     <button
-                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        className="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                                         type="button"
                                         onClick={() => setShowModal(false)}
                                     >
